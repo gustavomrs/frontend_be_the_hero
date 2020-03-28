c7ba4f2508554babf18865b2931ae6b2d4f69ea0
Gustavo Marques <gustavo@trackmob.com.br>
2020-03-28 10:37:04 -0300

    Add style to register form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -14,9 +14,9 @@ export default function Register() {
           <h1>Cadastro</h1>
           <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
 
-          <Link className="back-link" to="/register">
+          <Link className="back-link" to="/">
             <FiArrowLeft size="16"  color="#E02041"/>
-            Não tenho cadastro
+            Voltar para Login
           </Link>
         </section>
         <form>
