cccfcebfad7d427daed0ec0ce95bfe240c0c698a
Gustavo Marques <gustavoufms@gmail.com>
2020-04-12 18:34:38 -0300

    update backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://nodejs-backend-be-the-hero.herokuapp.com/ongs'
 })
 
 export default api
